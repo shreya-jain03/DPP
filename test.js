989208db8e78c676a8e8c66805c099739314516f
shreya-jain03 <shreyajain45395@gmail.com>
2026-08-05 11:19:32 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,15 +1,36 @@
 const fs = require("fs");
+let passed = true;
+console.log("Registration Test\n")
 
-let passed = "true";
-
-console.log("registration test started");
-
-if (fs.existsSync("index.html")) {
-    console.log("TC-01: index.html file exists - passed");
+if(fs.existsSync("index.html")) {
+   console.log("TC-01 : index.html exists:PASS");
 }
-else {
-    console.log("TC-01: index.html file does not exist - failed");
-    passed = "false";
+else{
+        console.log("TC-01 : index.html exists:FAIL");
+        passed=false;
+     }
+if(fs.existsSync("css/style.css")) {
+        console.log("TC-02 :style.css exists:PASS");
+     }
+else{
+    console.log("TC-02 : style.css exists:FAIL");
+    passed=false;
 }
-
-console.log("Test Result: " + passed);
+if(fs.existsSync("script.js")) {
+    console.log("TC-03 :script.jss exists:PASS");
+ }
+else{
+console.log("TC-03 : script.js exists:FAIL");
+passed=false;
+}
+if(fs.existsSync("data/students.json")) {
+    console.log("TC-4 : students.json exists:PASS");
+ }
+else{
+console.log("TC-04 : students.json exists:FAIL");
+passed=false;
+}
+const students = JSON.parse(
+    fs.readFileSync("data/students.json")
+);
+const student=student[0];
