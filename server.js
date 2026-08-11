3b5d40c08b1136dfed63e1ebfbe8bdeca96b202d
Shreya Jain <shreyajain45395@gmail.com>
2026-08-11 13:37:00 +0530

    server.js

--- server.js
+++ server.js
@@ -1,41 +1,97 @@
+// const express = require("express");
+// const fs = require("fs");
+
+// const app = express();
+// const PORT = 3000;
+
+// app.use(express.json());
+// app.use(express.static(__dirname));
+
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/shreya.html");
+// });
+
+// app.post("/register", (req, res) => {
+//     let students = [];
+
+//     if (fs.existsSync("students.json")) {
+//         const data = fs.readFileSync("students.json", "utf8");
+//         students = data ? JSON.parse(data) : [];
+//     }
+
+//     const newStudent = {
+//         name: req.body.name,
+//         email: req.body.email,
+//         mobile: req.body.phone,
+//         branch: req.body.course,
+//         password: req.body.password
+//     };
+
+//     students.push(newStudent);
+
+//     fs.writeFileSync(
+//         "students.json",
+//         JSON.stringify(students, null, 2)
+//     );
+
+//     res.json({
+//         success: true,
+//         message: "Registration successful!"
+//     });
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}`);
+// });
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static(__dirname));
 
-app.post("/register", (req, res) => {
-    let students = [];
+const filePath = path.join(__dirname, "student.json");
 
-    if (fs.existsSync("students.json")) {
-        const data = fs.readFileSync("students.json", "utf8");
-        students = data ? JSON.parse(data) : [];
+app.get("/students", (req, res) => {
+
+    if (!fs.existsSync(filePath)) {
+
+        fs.writeFileSync(filePath, JSON.stringify({ students: [] }, null, 2));
+
     }
 
-    const newStudent = {
-        name: req.body.name,
-        email: req.body.email,
-        mobile: req.body.phone,
-        branch: req.body.course,
-        password: req.body.password
-    };
+    const data = JSON.parse(fs.readFileSync(filePath));
 
-    students.push(newStudent);
+    res.json(data);
 
-    fs.writeFileSync(
-        "students.json",
-        JSON.stringify(students, null, 2)
-    );
-
-    res.json({
-        success: true,
-        message: "Registration successful!"
-    });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.post("/register", (req, res) => {
+
+    let data = { students: [] };
+
+    if (fs.existsSync(filePath)) {
+
+        data = JSON.parse(fs.readFileSync(filePath));
+
+    }
+
+    data.students.push(req.body);
+
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+
+    res.json({
+
+        message: "Registration Successful"
+
+    });
+
+});
+
+app.listen(3000, () => {
+
+    console.log("Server Running : http://localhost:3000");
+
 });
